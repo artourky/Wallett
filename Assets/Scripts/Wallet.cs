16a22525415f22b600ea5eb836f6450b28db3c05
Arshd <artourky@yahoo.com>
2016-07-17 13:45:09 +0200

    Nothing much, Just some UI and a script :P

---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Wallet : MonoBehaviour {
+
+    public double startMoney = 0.0d;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
